aac70f72e62643ea52ceb0d4403837a0037915f9
RaghavSitaula1 <144266737+RaghavSitaula1@users.noreply.github.com>
2023-09-06 23:05:48 +0545

    Add files via upload

--- FixedPointIteration.m
+++ FixedPointIteration.m
@@ -4,8 +4,6 @@ disp('Put @ (x) before writing a function')
 f=input('Enter a function f(x) : ');
 g=input('Enter a function g(x) : ');
 
-
-
 for j=0:10
    
         if (f(j)>0 && f(j+1)<0 ) || (f(j)<0 &&  f(j+1)>0) 
@@ -21,14 +19,12 @@ for j=0:10
 
 end
 
-
-
 disp("Root lies between : ")
 A=sprintf("[%d,%d]",p,q);
 disp(A);
 
 n=input ('Enter no. of iterations : ');
-x0=input ('Enter Your intial guess : ');
+x0=input ('Enter Your initial guess : ');
 e=input('Enter Tolerance : ');
 
 row=sprintf('Iterations    X                 Error');
@@ -36,13 +32,16 @@ disp(row);
 for i=1:n
     DF=diff(g,x);
     values=subs(DF,x0);
+
     if (abs(values))>1
         errordlg("Iterative method is not convergent")
         return
     end
+
     x1=g(x0);
     print=sprintf('%3d          %11.10f       %11.10f',i,x1,abs(x1-x0));
     disp(print);
+
     if abs(x1-x0)<e
         disp('The Root of the equation is')
         z=sprintf("%.10f",x1);
@@ -59,7 +58,6 @@ Y=@(x) x;
 hold on
 fplot(g,[p,q],'b-','LineWidth',2)
 fplot(Y ,[p,q],'r-','LineWidth',2)
-
 title('Fixed Point Iteration Converging')
 
 hold on
